ae3a673a271b5de7d719ec9d97aaecd16818fb17
Gui-alme <guilherme.almeida@sptech.school>
2022-11-27 23:02:19 -0300

    faltando somente os slides na página de inGame

--- site/app.js
+++ site/app.js
@@ -8,10 +8,7 @@ var PORTA = process.env.AMBIENTE_PROCESSO == "desenvolvimento" ? 3333 : 8080;
 
 var app = express();
 
-var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
-var avisosRouter = require("./src/routes/avisos");
-var medidasRouter = require("./src/routes/medidas");
 var quizRouter = require("./src/routes/quiz");
 var avaliacaoRouter = require("./src/routes/avaliacao");
 
@@ -21,10 +18,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cors());
 
-app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
-app.use("/avisos", avisosRouter);
-app.use("/medidas", medidasRouter);
 app.use("/quiz", quizRouter);
 app.use("/avaliacao", avaliacaoRouter);
 
